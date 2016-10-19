ba6ec9efc4176c98d0d52efb7835f6b6c5f19857
anubhav khanna <akhanna@sapient.com>
2016-10-19 12:29:09 +0100

    testing ashish

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Welcome to innovation platform')
+  response.send('Welcome to Ashish on testing the platform for the first time')
 })
 
 app.listen(app.get('port'), function() {
